b6c72ec0b910005d8682dd45ae9646ebc46b2d1c
Shiven Chahar <shivenchahar15266@gmail.com>
2025-07-21 18:21:11 +0530

    Implemented Post for createUser

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,21 @@
 const mongoose = require('mongoose');
 
-const mongoURI = 'mongodb://127.0.0.1:27017/mydb';
+const mongoURI = 'mongodb+srv://ShivenChahar:shiven0001@foodbuzzz.pweyvfn.mongodb.net/foodBuzzzMERN?retryWrites=true&w=majority&appName=foodBuzzz';  
 
 const mongoDB = () => {
   mongoose.connect(mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(() => {
+  .then(async () => {
     console.log('Connected to MongoDB');
+
+    try {
+      const fetched_data = await mongoose.connection.db.collection('food_items').find({}).toArray();
+      console.log('Fetched Data:', /*fetched_data*/);
+    } catch (err) {
+      console.error('Error fetching data:', err);
+    }
   })
   .catch((err) => {
     console.error('MongoDB connection error:', err);
@@ -17,4 +24,3 @@ const mongoDB = () => {
 };
 
 module.exports = mongoDB;
- 
--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,8 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.use(express.json())
+app.use('/api', require("./Routes/createUser"));
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
